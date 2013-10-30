d970a1cff62cd3d0ce583027e6d0fa73bdc8556a
Demis Bellot <demis.bellot@gmail.com>
2013-10-30 06:50:59 -0400

    refactor linq_quantifiers.clj

--- src/clj_linq/linq_quantifiers.clj
+++ src/clj_linq/linq_quantifiers.clj
@@ -3,47 +3,34 @@
 
 ;; linq67: Any - Simple
 (defn linq67 []
-  (def words ["believe" "relief" "receipt" "field"])
-
-  (def i-after-e (some #(.contains % "ie") words))
-
-  (println "There is a word that contains in the list that contains 'ei':" i-after-e)
-)
+  (let [words ["believe" "relief" "receipt" "field"]
+        i-after-e (some #(.contains % "ie") words)]
+    (println "There is a word that contains in the list that contains 'ei':" i-after-e)))
 
 ;; linq69: Any - Grouped
 (defn linq69 []
-  (def products products-list)
-
-  (def product-groups
-    (->> products
-         (group-by :category)
-         (filter #(some (fn [p] (= (:units-in-stock p) 0)) (get % 1)))
-         (map #(hash-map :category (get % 0), :products (get % 1)))))
-
-  (doall (map println product-groups))
-)
+  (let [products products-list
+        product-groups
+        (->> products
+             (group-by :category)
+             (filter #(some (fn [p] (= (:units-in-stock p) 0)) (get % 1)))
+             (map #(identity {:category (first %), :products (second %)})))]
+    (doseq [x product-groups] (println x))))
 
 ;; linq70: All - Simple
 (defn linq70 []
-  (def numbers [1 11 3 19 41 65 19])
-
-  (def only-odd (every? #(= (mod % 2) 1) numbers))
-
-  (println "The list contains only odd numbers:" only-odd)
-)
+  (let [numbers [1 11 3 19 41 65 19]
+        only-odd (every? #(= (mod % 2) 1) numbers)]
+    (println "The list contains only odd numbers:" only-odd)))
 
 ;; linq72: All - Grouped
 (defn linq72 []
-  (def products products-list)
-
-  (def product-groups
-    (->> products
-         (group-by :category)
-         (filter #(every? (fn [p] (> (:units-in-stock p) 0)) (get % 1)))
-         (map #(hash-map :category (get % 0), :products (get % 1)))
-         ))
-
-  (doall (map println product-groups))
-)
+  (let [products products-list
+        product-groups
+        (->> products
+             (group-by :category)
+             (filter #(every? (fn [p] (> (:units-in-stock p) 0)) (second %)))
+             (map #(identity {:category (first %), :products (second %)})))]
+    (doseq [x product-groups] (println x))))
 
 (def examples [linq67 linq69 linq70 linq72])
