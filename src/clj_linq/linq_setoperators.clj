64e7ff73978a878c00ce9f5d3142f2cfbbeebe2f
Demis Bellot <demis.bellot@gmail.com>
2013-10-30 06:35:09 -0400

    refactor linq_setoperators

--- src/clj_linq/linq_setoperators.clj
+++ src/clj_linq/linq_setoperators.clj
@@ -5,97 +5,72 @@
 
 ;; linq46: Distinct - 1
 (defn linq46 []
-  (def factors-of-300 [2, 2, 3, 5, 5])
-  (def unique-factors (distinct factors-of-300))
-
-  (println "Prime factors of 300:")
-  (doall (map println unique-factors))
-)
+  (let [factors-of-300 [2, 2, 3, 5, 5]
+        unique-factors (distinct factors-of-300)]
+    (println "Prime factors of 300:")
+    (doseq [n unique-factors] (println n))))
 
 ;; linq47: Distinct - 2
 (defn linq47 []
-  (def products products-list)
-  (def category-names (->> products
-                           (map :category)
-                           distinct))
-
-  (println "Category names:")
-  (doall (map println category-names))
-)
+  (let [products products-list
+        category-names (->> products
+                            (map :category)
+                            distinct)]
+    (println "Category names:")
+    (doseq [c category-names] (println c))))
 
 ;; linq48: Union - 1
 (defn linq48 []
-  (def numbers-a [0 2 4 5 6 8 9])
-  (def numbers-b [1 3 5 7 8])
-
-  (def unique-numbers (union (set numbers-a) (set numbers-b)))
-
-  (println "Unique numbers from both arrays:")
-  (doall (map println unique-numbers))
-)
+  (let [numbers-a [0 2 4 5 6 8 9]
+        numbers-b [1 3 5 7 8]
+        unique-numbers (union (set numbers-a) (set numbers-b))]
+    (println "Unique numbers from both arrays:")
+    (doseq [n unique-numbers] (println n))))
 
 ;; linq49: Union - 2
 (defn linq49 []
-  (def products products-list)
-  (def customers customers-list)
-
-  (def product-first-chars (map #(get (:product-name %) 0) products))
-  (def customer-first-chars (map #(get (:company-name %) 0) customers))
-
-  (def unique-first-chars (union (set product-first-chars) (set customer-first-chars)))
-
-  (println "Unique first letters from Product names and Customer names:")
-  (doall (map println unique-first-chars))
-)
+  (let [products products-list
+        customers customers-list
+        product-first-chars (map #(get (:product-name %) 0) products)
+        customer-first-chars (map #(get (:company-name %) 0) customers)
+        unique-first-chars (union (set product-first-chars) (set customer-first-chars))]
+    (println "Unique first letters from Product names and Customer names:")
+    (doseq [x unique-first-chars] (println x))))
 
 ;; linq50: Intersect - 1
 (defn linq50 []
-  (def numbers-a [0 2 4 5 6 8 9])
-  (def numbers-b [1 3 5 7 8])
-
-  (def common-numbers (intersection (set numbers-a) (set numbers-b)))
-
-  (println "Common numbers shared by both arrays:")
-  (doall (map println common-numbers))
-)
+  (let [numbers-a [0 2 4 5 6 8 9]
+        numbers-b [1 3 5 7 8]
+        common-numbers (intersection (set numbers-a) (set numbers-b))]
+    (println "Common numbers shared by both arrays:")
+    (doseq [n common-numbers] (println n))))
 
 ;; linq51: Intersect - 2
 (defn linq51 []
-  (def products products-list)
-  (def customers customers-list)
-
-  (def product-first-chars (map #(get (:product-name %) 0) products))
-  (def customer-first-chars (map #(get (:company-name %) 0) customers))
-
-  (def common-first-chars (intersection (set product-first-chars) (set customer-first-chars)))
-
-  (println "Common first letters from Product names and Customer names:")
-  (doall (map println common-first-chars))
-)
+  (let [products products-list
+        customers customers-list
+        product-first-chars (map #(get (:product-name %) 0) products)
+        customer-first-chars (map #(get (:company-name %) 0) customers)
+        common-first-chars (intersection (set product-first-chars) (set customer-first-chars))]
+    (println "Common first letters from Product names and Customer names:")
+    (doseq [x common-first-chars] (println x))))
 
 ;; linq52: Except - 1
 (defn linq52 []
-  (def numbers-a [0 2 4 5 6 8 9])
-  (def numbers-b [1 3 5 7 8])
-
-  (def a-only-numbers (difference (set numbers-a) (set numbers-b)))
-
+  (let [numbers-a [0 2 4 5 6 8 9]
+        numbers-b [1 3 5 7 8]
+        a-only-numbers (difference (set numbers-a) (set numbers-b))]
   (println "Numbers in first array but not second array:")
-  (doall (map println a-only-numbers))
-)
+  (doseq [n a-only-numbers] (println n))))
 
 ;; linq53: Except - 2
 (defn linq53 []
-  (def products products-list)
-  (def customers customers-list)
-
-  (def product-first-chars (map #(get (:product-name %) 0) products))
-  (def customer-first-chars (map #(get (:company-name %) 0) customers))
-
-  (def product-only-first-chars (difference (set product-first-chars) (set customer-first-chars)))
-
-  (println "First letters from Product names, but not from Customer names:")
-  (doall (map println product-only-first-chars))
-)
+  (let [products products-list
+        customers customers-list
+        product-first-chars (map #(get (:product-name %) 0) products)
+        customer-first-chars (map #(get (:company-name %) 0) customers)
+        product-only-first-chars (difference (set product-first-chars) (set customer-first-chars))]
+    (println "First letters from Product names, but not from Customer names:")
+    (doseq [x  product-only-first-chars] (println x))))
 
 (def examples [linq46 linq47 linq48 linq49 linq50 linq51 linq52 linq53])
