7443760bb996a90b0f613652e2f65861a0039e76
Demis Bellot <demis.bellot@gmail.com>
2013-10-30 06:39:59 -0400

    refactor linq_conversions

--- src/clj_linq/linq_conversions.clj
+++ src/clj_linq/linq_conversions.clj
@@ -3,44 +3,35 @@
 
 ;; linq54: ToArray
 (defn linq54 []
-  (def dbls [1.7 2.3 1.9 4.1 2.9])
-  (def sorted-doubles (->> dbls sort reverse))
-
-  (println "Every other double from highest to lowest:")
-  (doall (map println (take-nth 2 sorted-doubles)))
-)
+  (let [dbls [1.7 2.3 1.9 4.1 2.9]
+        sorted-doubles (->> dbls sort reverse)]
+    (println "Every other double from highest to lowest:")
+    (doseq [d (take-nth 2 sorted-doubles)] (println d))))
 
 ;;linq55: ToList
 (defn linq55 []
-  (def words ["cherry", "apple", "blueberry"])
-  (def sorted-words (->> words
-                         sort
-                         (apply list)))
-
-  (println "The sorted word list:")
-  (doall (map println sorted-words))
-)
+  (let [words ["cherry", "apple", "blueberry"]
+        sorted-words (->> words
+                          sort
+                          (apply list))]
+    (println "The sorted word list:")
+    (doseq [w sorted-words] (println w))))
 
 ;;linq56: ToDictionary
 (defn linq56 []
-  (def sorted-records [{:name "Alice", :score 50}
-                       {:name "Bob", :score 40}
-                       {:name "Cathy", :score 45}])
-
-  (def sorted-records-dict (->> sorted-records
-                                (map #(hash-map (:name %) (:score %)))
-                                (into {})))
-  (println "Bob's score:" (sorted-records-dict "Bob"))
-)
+  (let [sorted-records [{:name "Alice", :score 50}
+                        {:name "Bob", :score 40}
+                        {:name "Cathy", :score 45}]
+        sorted-records-dict (->> sorted-records
+                                 (map #(hash-map (:name %) (:score %)))
+                                 (into {}))]
+    (println "Bob's score:" (sorted-records-dict "Bob"))))
 
 ;; linq57: OfType
 (defn linq57 []
-  (def numbers [nil 1.0 "two" 3 "four" 5 "six" 7.0])
-
-  (def dbls (filter #(= (type %) java.lang.Double) numbers))
-
-  (println "Numbers stored as doubles:")
-  (doall (map println dbls))
-)
+  (let [numbers [nil 1.0 "two" 3 "four" 5 "six" 7.0]
+        dbls (filter #(= (type %) java.lang.Double) numbers)]
+    (println "Numbers stored as doubles:")
+    (doseq [d dbls] (println d))))
 
 (def examples [linq54 linq55 linq56 linq57])
