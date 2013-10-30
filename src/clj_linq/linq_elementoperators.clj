4a9a47abde243226dffc6d21fa2900d7cf6245b8
Demis Bellot <demis.bellot@gmail.com>
2013-10-30 06:43:06 -0400

    refactor elementoperators.clj

--- src/clj_linq/linq_elementoperators.clj
+++ src/clj_linq/linq_elementoperators.clj
@@ -3,54 +3,39 @@
 
 ;; linq58: First - Simple
 (defn linq58 []
-  (def products products-list)
-
-  (def product-12 (->> products
-                      (filter #(= (:product-id %) 12))
-                       first))
-
-  (println product-12)
-)
+  (let [products products-list
+        product-12 (->> products
+                        (filter #(= (:product-id %) 12))
+                        first)]
+    (println product-12)))
 
 ;; linq59: First - Condition
 (defn linq59 []
-  (def strings ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"])
-
-  (def starts-with-o (first (filter #(= (get % 0) \o) strings)))
-
-  (println "A string starting with 'o':" starts-with-o)
-)
+  (let [strings ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+        starts-with-o (first (filter #(= (get % 0) \o) strings))]
+    (println "A string starting with 'o':" starts-with-o)))
 
 ;; linq61: FirstOrDefault - Simple
 (defn linq61 []
-  (def numbers [])
-
-  (def first-num-or-default (get numbers 0 0))
-
-  (println first-num-or-default)
-)
+  (let [numbers []
+        first-num-or-default (get numbers 0 0)]
+    (println first-num-or-default)))
 
 ;; linq62: FirstOrDefault - Condition
 (defn linq62 []
-  (def products products-list)
-
-  (def product-789 (->> products
-                      (filter #(= (:product-id %) 789))
-                       first))
-
-  (println "Product 789 exists:" (not= product-789 nil))
-)
+  (let [products products-list
+        product-789 (->> products
+                         (filter #(= (:product-id %) 789))
+                         first)]
+    (println "Product 789 exists:" (not= product-789 nil))))
 
 ;; linq64: ElementAt
 (defn linq64 []
-  (def numbers [5 4 1 3 9 8 6 7 2 0])
-
-  (def fourth-low-num (->> numbers
-                      (filter #(> % 5))
-                       second))
-
-  (println "Second number > 5:" fourth-low-num)
-)
+  (let [numbers [5 4 1 3 9 8 6 7 2 0]
+        fourth-low-num (->> numbers
+                            (filter #(> % 5))
+                            second)]
+    (println "Second number > 5:" fourth-low-num)))
 
 (def examples [linq58 linq59 linq61 linq62 linq64])
 
