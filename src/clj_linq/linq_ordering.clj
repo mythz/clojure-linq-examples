093f2b42270cadf358119272778095f2c05d2410
Demis Bellot <demis.bellot@gmail.com>
2013-10-30 06:06:44 -0400

    refactored linq_ordering.clj

--- src/clj_linq/linq_ordering.clj
+++ src/clj_linq/linq_ordering.clj
@@ -18,112 +18,86 @@
 
 ;; linq28: SkipWhile - Simple
 (defn linq28 []
-  (def words ["cherry" "apple" "blueberry"])
-  (def sorted-words (sort words))
-
-  (println "The sorted list of words:")
-  (doall (map println sorted-words))
-)
+  (let [words ["cherry" "apple" "blueberry"]
+        sorted-words (sort words)]
+    (println "The sorted list of words:")
+    (doseq [w sorted-words] (println w))))
 
 ;; linq29: OrderBy - Simple 2
 (defn linq29 []
-  (def words ["cherry" "apple" "blueberry"])
-  (def sorted-words (sort-by count words))
-
-  (println "The sorted list of words (by length):")
-  (doall (map println sorted-words))
-)
+  (let [words ["cherry" "apple" "blueberry"]
+        sorted-words (sort-by count words)]
+    (println "The sorted list of words (by length):")
+    (doseq [w sorted-words] (println w))))
 
 ;; linq30: OrderBy - Simple 3
 (defn linq30 []
-  (def products products-list)
-  (def sorted-products (sort-by :product-name products))
-
-  (doall (map println sorted-products))
-)
+  (let [products products-list
+        sorted-products (sort-by :product-name products)]
+    (doseq [p sorted-products] (println p))))
 
 ;; linq31: OrderBy - Comparer
 (defn linq31 []
-  (def words ["aPPLE" "AbAcUs" "bRaNcH" "BlUeBeRrY" "ClOvEr" "cHeRry"])
-  (def sorted-words (sort-by identity case-insensitive-compare words))
-
-  (doall (map println sorted-words))
-)
+  (let [words ["aPPLE" "AbAcUs" "bRaNcH" "BlUeBeRrY" "ClOvEr" "cHeRry"]
+        sorted-words (sort-by identity case-insensitive-compare words)]
+    (doseq [w sorted-words] (println w))))
 
 ;; linq32: OrderByDescending - Simple 1
 (defn linq32 []
-  (def dbls [1.7 2.3 1.9 4.1 2.9])
-  (def sorted-doubles (reverse (sort dbls)))
-
-  (println "The doubles from highest to lowest:")
-  (doall (map println sorted-doubles))
-)
+  (let [dbls [1.7 2.3 1.9 4.1 2.9]
+        sorted-doubles (reverse (sort dbls))]
+    (println "The doubles from highest to lowest:")
+    (doseq [d sorted-doubles] (println d))))
 
 ;; linq33: OrderByDescending - Simple 2
 (defn linq33 []
-  (def products products-list)
-  (def sorted-products (reverse (sort-by :units-in-stock products)))
-
-  (doall (map println sorted-products))
-)
+  (let [products products-list
+        sorted-products (reverse (sort-by :units-in-stock products))]
+    (doseq [p sorted-products] (println p))))
 
 ;; linq34: OrderByDescending - Comparer
 (defn linq34 []
-  (def words ["aPPLE" "AbAcUs" "bRaNcH" "BlUeBeRrY" "ClOvEr" "cHeRry"])
-  (def sorted-words (->> words
-                         (sort-by identity case-insensitive-compare)
-                         reverse))
-
-  (doall (map println sorted-words))
-)
+  (let [words ["aPPLE" "AbAcUs" "bRaNcH" "BlUeBeRrY" "ClOvEr" "cHeRry"]
+        sorted-words (->> words
+                          (sort-by identity case-insensitive-compare)
+                          reverse)]
+    (doseq [w sorted-words] (println w))))
 
 ;; linq35: ThenBy - Simple
 (defn linq35 []
-  (def digits ["zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine"])
-
-  (def sorted-digits (order-by [count identity] digits))
-
-  (println "Sorted digits:")
-  (doall (map println sorted-digits))
-)
+  (let [digits ["zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine"]
+        sorted-digits (order-by [count identity] digits)]
+    (println "Sorted digits:")
+    (doseq [d sorted-digits] (println d))))
 
 ;; linq36: ThenBy - Comparer
 (defn linq36 []
-  (def words ["aPPLE" "AbAcUs" "bRaNcH" "BlUeBeRrY" "ClOvEr" "cHeRry"])
-  (def sorted-words (order-by [count (fn [x] (.toLowerCase x))] words))
-
-  (doall (map println sorted-words))
-)
+  (let [words ["aPPLE" "AbAcUs" "bRaNcH" "BlUeBeRrY" "ClOvEr" "cHeRry"]
+        sorted-words (order-by [count (fn [x] (.toLowerCase x))] words)]
+    (doseq [w sorted-words] (println w))))
 
 ;; linq37: ThenByDescending - Simple
 (defn linq37 []
-  (def products products-list)
-  (def sorted-products (order-by [:category #(* -1 (:unit-price  %))] products))
-
-  (doall (map println sorted-products))
-)
+  (let [products products-list
+        sorted-products (order-by [:category #(* -1 (:unit-price  %))] products)]
+    (doseq [p sorted-products] (println p))))
 
 ;; linq38: ThenByDescending - Comparer
 (defn linq38 []
-  (def words ["aPPLE" "AbAcUs" "bRaNcH" "BlUeBeRrY" "ClOvEr" "cHeRry"])
-  (def sorted-words (order-by-comparers
-                     [#(compare (count %1) (count %2))
-                      #(case-insensitive-compare %2 %1)]
-                     words))
-
-  (doall (map println sorted-words))
-)
+  (let [words ["aPPLE" "AbAcUs" "bRaNcH" "BlUeBeRrY" "ClOvEr" "cHeRry"]
+        sorted-words (order-by-comparers
+                      [#(compare (count %1) (count %2))
+                       #(case-insensitive-compare %2 %1)]
+                      words)]
+    (doseq [w sorted-words] (println w))))
 
 ;; linq39: Reverse
 (defn linq39 []
-  (def digits ["zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine"])
-
-  (def sorted-digits (->> digits
-                          (filter #(= (get % 1) \i))
-                          reverse))
-
-  (println "A backwards list of the digits with a second character of 'i':")
-  (doall (map println sorted-digits))
-)
+  (let [digits ["zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine"]
+        sorted-digits (->> digits
+                           (filter #(= (get % 1) \i))
+                           reverse)]
+    (println "A backwards list of the digits with a second character of 'i':")
+    (doseq [d sorted-digits] (println d))))
 
 (def examples [linq28 linq29 linq30 linq31 linq32 linq33 linq34 linq35 linq36 linq37 linq38 linq39])
