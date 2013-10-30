c2c07a57795623d9545d2319b413bcac30094c16
Demis Bellot <demis.bellot@gmail.com>
2013-10-30 06:54:21 -0400

    refactor linq_miscoperators.clj

--- src/clj_linq/linq_miscoperators.clj
+++ src/clj_linq/linq_miscoperators.clj
@@ -3,47 +3,34 @@
 
 ;; linq94: Concat - 1
 (defn linq94 []
-  (def numbers-a [0 2 4 5 6 8 9])
-  (def numbers-b [1 3 5 7 8])
-
-  (def all-numbers (flatten [numbers-a numbers-b]))
-
-  (println "All numbers from both arrays:")
-  (doall (map println all-numbers))
-)
+  (let [numbers-a [0 2 4 5 6 8 9]
+        numbers-b [1 3 5 7 8]
+        all-numbers (flatten [numbers-a numbers-b])]
+    (println "All numbers from both arrays:")
+    (doseq [n all-numbers] (println n))))
 
 ;; linq95: Concat - 2
 (defn linq95 []
-  (def products products-list)
-  (def customers customers-list)
-
-  (def customer-names (map :company-name customers))
-  (def product-names (map :product-name products))
-
-  (def all-names (flatten [customer-names product-names]))
-
-  (println "Customer and product names:")
-  (doall (map println all-names))
-)
+  (let [products products-list
+        customers customers-list
+        customer-names (map :company-name customers)
+        product-names (map :product-name products)
+        all-names (flatten [customer-names product-names])]
+    (println "Customer and product names:")
+    (doseq [x all-names] (println x))))
 
 ;; linq96: EqualAll - 1
 (defn linq96 []
-  (def words-a ["cherry" "apple" "blueberry"])
-  (def words-b ["cherry" "apple" "blueberry"])
-
-  (def match (= words-a words-b))
-
-  (println "The sequences match:" match)
-)
+  (let [words-a ["cherry" "apple" "blueberry"]
+        words-b ["cherry" "apple" "blueberry"]
+        match (= words-a words-b)]
+    (println "The sequences match:" match)))
 
 ;; linq97: EqualAll - 2
 (defn linq97 []
-  (def words-a ["cherry" "apple" "blueberry"])
-  (def words-b ["apple" "blueberry" "cherry"])
-
-  (def match (= words-a words-b))
-
-  (println "The sequences match:" match)
-)
+  (let [words-a ["cherry" "apple" "blueberry"]
+        words-b ["apple" "blueberry" "cherry"]
+        match (= words-a words-b)]
+    (println "The sequences match:" match)))
 
 (def examples [linq94 linq95 linq96 linq97])
